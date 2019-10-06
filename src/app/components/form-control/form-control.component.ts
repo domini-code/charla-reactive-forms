14546996d5af0bd8ad461ec90645eca70daa313a
Bezael <bezael_e@hotmail.com>
2019-10-06 11:34:27 -0400

    FormControl

--- src/app/components/form-control/form-control.component.ts
+++ src/app/components/form-control/form-control.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   template: `
-    <input type="text" value="HolaMundo" />
+    <input type="text" [formControl]="user" />
+    <p>Value: {{ user.value }}</p>
   `,
   styleUrls: ['./form-control.component.css']
 })
 export class FormControlComponent implements OnInit {
+  user = new FormControl('HolaMundito');
   constructor() {}
 
   ngOnInit() {}
