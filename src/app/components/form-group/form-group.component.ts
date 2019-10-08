c4f0b4cede15f0a2961bd2412f54d49b518a7e0b
Bezael <bezael_e@hotmail.com>
2019-10-08 08:40:50 -0400

    FormGroup

---
+++ src/app/components/form-group/form-group.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-group',
+  templateUrl: './form-group.component.html',
+  styleUrls: ['./form-group.component.css']
+})
+export class FormGroupComponent implements OnInit {
+  myForm: FormGroup;
+  constructor() {}
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+}
